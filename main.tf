b58407780824a46693407aae0901037e4acafb04
Arne Nilsen <arne.nilsen@skatteetaten.no>
2025-06-08 09:50:14 +0200

    Automating Kafka setup

---
+++ main.tf
@@ -0,0 +1,125 @@
+
+resource "kubernetes_service" "kafka_service" {
+  metadata {
+    name = "kafka-service"
+    labels = {
+      app = "kafka-app"
+    }
+  }
+
+  spec {
+    selector = {
+      app = "kafka-app"
+    }
+
+    port {
+      name       = "kafka-port1"
+      port       = 9092
+      target_port = 9092
+      protocol   = "TCP"
+    }
+
+    port {
+      name       = "kafka-port2"
+      port       = 9093
+      target_port = 9093
+      protocol   = "TCP"
+    }
+  }
+}
+
+variable "ORDINAL_NUMBER" {
+  default = ""
+}
+resource "kubernetes_stateful_set" "kafka" {
+  metadata {
+    name = "kafka"
+    labels = {
+      app = "kafka-app"
+    }
+  }
+
+  spec {
+    service_name = "kafka-service"
+    replicas     = 1
+
+    selector {
+      match_labels = {
+        app = "kafka-app"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "kafka-app"
+        }
+      }
+
+      spec {
+        container {
+          name  = "kafka"
+          image = "docker.io/confluentinc/confluent-local:latest"
+          image_pull_policy = "IfNotPresent"
+
+          port {
+            container_port = 29092
+          }
+
+          port {
+            container_port = 9092
+          }
+
+          port {
+            container_port = 9093
+          }
+
+          env {
+            name = "ORDINAL_NUMBER"
+            value_from {
+              field_ref {
+                field_path = "metadata.labels['apps.kubernetes.io/pod-index']"
+              }
+            }
+          }
+
+          env {
+            name  = "KAFKA_NODE_ID"
+            value = "$(ORDINAL_NUMBER)"
+          }
+          env {
+            name  = "CLUSTER_ID"
+            value = "ODhCODhFMjEyNDZCNEI0ME"
+          }
+          env {
+            name = "POD_NAME"
+            value = "kafka-$(ORDINAL_NUMBER)"
+          }
+          env {
+            name  = "KAFKA_LISTENERS"
+            value = "CONTROLLER://:29092,PLAINTEXT://:9092,PLAINTEXT_HOST://:9093"
+          }
+
+          env {
+            name = "KAFKA_CONTROLLER_QUORUM_VOTERS"
+            value = "$(ORDINAL_NUMBER)@$(POD_NAME).kafka-service.default.svc.cluster.local:29092"
+          }
+          # env {
+          #   name  = "KAFKA_CONTROLLER_QUORUM_VOTERS"
+          #   value = "0@kafka-0.kafka-service.default.svc.cluster.local:29092"
+          # }
+
+          env {
+            name  = "KAFKA_ADVERTISED_LISTENERS"
+            value = "PLAINTEXT://localhost:9092,PLAINTEXT_HOST://kafka-service:9093"
+          }
+
+          env {
+            name  = "KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR"
+            value = "1"
+          }
+        }
+      }
+    }
+  }
+}
---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
+
+variable "kube_config_path" {
+  default = "~/.kube/config"
+}
